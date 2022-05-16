d846ee6584758e17fc8d02b449e211aa7772dd3f
IsabelaCJesus <isabelac396@gmail.com>
2022-05-16 12:42:33 -0300

    Crud de paciente

--- src/app/modal-paciente/modal-paciente.page.ts
+++ src/app/modal-paciente/modal-paciente.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Paciente, DataPacienteService } from '../services/data-paciente.service';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-paciente',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalPacientePage implements OnInit {
 
-  constructor() { }
-
+  @Input() id: string;
+  paciente: Paciente = null;
+ 
+  constructor(private dataService: DataPacienteService, private modalCtrl: ModalController, private toastCtrl: ToastController) { }
+ 
   ngOnInit() {
+    this.dataService.getPacienteById(this.id).subscribe(res => {
+      this.paciente = res;
+    });
   }
-
+ 
+  async deletarPaciente() {
+    await this.dataService.deletePaciente(this.paciente);
+    const toast = await this.toastCtrl.create({
+      message: 'Exclusão realizada com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+    this.modalCtrl.dismiss();
+  }
+ 
+  async atualizarPaciente() {
+    await this.dataService.updatePaciente(this.paciente);
+    const toast = await this.toastCtrl.create({
+      message: 'Dados atualizados com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+    this.modalCtrl.dismiss();
+  }
 }
